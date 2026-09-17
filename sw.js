5f6bc4e0739499b8ba9e13d7bd1aef4b830dda36
EndemiasBR <niljcs@gmail.com>
2026-09-17 12:47:20 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = "endemiascampo-v1";
+const ARQUIVOS = ["./", "./index.html", "./manifest.json"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ARQUIVOS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
